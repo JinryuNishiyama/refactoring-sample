0f06d4bbab55109a6d7ace5b9936d5833e949bb6
meteorol50 <meteorol50.sky@gmail.com>
2024-06-08 10:58:47 +0900

    refactor: ループをreduce関数に置き換え

--- src/js/refactored.js
+++ src/js/refactored.js
@@ -45,11 +45,7 @@ function statement(invoice, plays) {
     return result;
   }
   function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
   function volumeCreditsFor(performance) {
     let result = 0;
@@ -59,11 +55,7 @@ function statement(invoice, plays) {
     return result;
   }
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 
   const statementData = {
