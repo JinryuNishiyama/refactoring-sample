5d9b951fe1bc967cbaffd3f008049f694cb90c2e
meteorol50 <meteorol50.sky@gmail.com>
2024-06-08 11:28:28 +0900

    feat: HTML出力関数を作成

--- src/js/refactor/statement.js
+++ src/js/refactor/statement.js
@@ -3,13 +3,30 @@ const plays = require("../../data/plays.json");
 
 const createStatementData = require("./createStatementData.js");
 
-function renderPlainText(data) {
-  function usd(targetNumber) {
-    return new Intl.NumberFormat("en-US",
-                        { style: "currency", currency: "USD",
-                          minimumFractionDigits: 2 }).format(targetNumber/100);
-  }
+function usd(targetNumber) {
+  return new Intl.NumberFormat("en-US",
+                      { style: "currency", currency: "USD",
+                        minimumFractionDigits: 2 }).format(targetNumber/100);
+}
 
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr></tr><th>play</th><th>seats</th><th>costs</th></tr>\n";
+  for (let perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  }
+  return result;
+}
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
     // 注文の内訳を出力
@@ -19,10 +36,12 @@ function renderPlainText(data) {
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
 }
-
 function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
 const result = statement(invoice, plays);
 console.log(result);
+
+const html = htmlStatement(invoice, plays);
+console.log(html);
