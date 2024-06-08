d006d0400ead880c4a1b0433a2afbaa5495c2a60
meteorol50 <meteorol50.sky@gmail.com>
2024-06-08 09:59:59 +0900

    refactor: format関数をドル表記を取得する関数に変更

--- src/js/refactored.js
+++ src/js/refactored.js
@@ -41,19 +41,19 @@ function statement(invoice, plays) {
     return result;
   }
 
-  function format(targetNumber) {
+  function usd(targetNumber) {
     return new Intl.NumberFormat("en-US",
                         { style: "currency", currency: "USD",
-                          minimumFractionDigits: 2 }).format(targetNumber);
+                          minimumFractionDigits: 2 }).format(targetNumber/100);
   }
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
     // 注文の内訳を出力
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount/100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
